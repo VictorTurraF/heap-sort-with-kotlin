eaf6898d8e28d29356723e9cec694f74944cc62c
Victor Turra <victor.florencio@univem.edu.br>
2024-03-14 18:16:16 -0300

    feat: finish binary heap tree insertion

--- src/main/kotlin/BinaryHeapTree.kt
+++ src/main/kotlin/BinaryHeapTree.kt
@@ -1,31 +1,49 @@
 package org.example
 
-data class Node(val index: Int, val value: Int)
+data class Item(val index: Int, var value: Int)
 
 class BinaryHeapTree {
     private var items: MutableList<Int> = mutableListOf();
 
     fun insert(item: Int) {
-        if (this.items.isEmpty())
-            this.items.add(item)
-        else {
-            val parent = this.getParent(items.size)
+        this.items.add(item)
+        this.checkParentsAndSwapIfNecessary(this.getLastItem())
+    }
 
-            if (item > parent.value) {
-                this.items.add(parent.value)
-                this.items[parent.index] = item
-            } else {
-                this.items.add(item)
-            }
+    private fun checkParentsAndSwapIfNecessary(currentItem: Item) {
+        if (currentItem.index == 0)
+            return
+
+        val parentItem = this.getParent(currentItem.index)
+
+        if (currentItem.value > parentItem.value) {
+            this.swap(currentItem, parentItem)
+            this.checkParentsAndSwapIfNecessary(parentItem)
         }
     }
 
-    private fun getParent(index: Int): Node {
+    private fun getLastItem(): Item {
+        val lastIndex = this.items.size - 1
+        val lastValue = this.items[lastIndex]
+
+        return Item(lastIndex, lastValue)
+    }
+
+    private fun getParent(index: Int): Item {
         // Return an object with index and value
         val parentIndex = (index - 1)/2
         val parentValue = this.items[parentIndex]
 
-        return Node(parentIndex, parentValue);
+        return Item(parentIndex, parentValue)
+    }
+
+    private fun swap(item: Item, parent: Item) {
+        this.items[parent.index] = item.value
+        this.items[item.index] = parent.value
+
+        val aux = item.value
+        item.value = parent.value
+        parent.value = aux
     }
 
     fun get(): List<Int> {
--- src/test/kotlin/app/BinaryHeapTreeTest.kt
+++ src/test/kotlin/app/BinaryHeapTreeTest.kt
@@ -33,4 +33,30 @@ class BinaryHeapTreeTest {
         assertNotEquals(listOf(4, 2, 8), tree.get())
         assertEquals(listOf(8, 2, 4), tree.get())
     }
+
+    @Test
+    fun testMultipleSwaps() {
+        val tree = BinaryHeapTree()
+
+        tree.insert(4)
+        tree.insert(2)
+        tree.insert(8)
+        tree.insert(10)
+
+        assertEquals(listOf(10, 8, 4, 2), tree.get())
+    }
+
+    @Test
+    fun testManyItemsInsertion() {
+        val tree = BinaryHeapTree()
+
+        tree.insert(4)
+        tree.insert(2)
+        tree.insert(8)
+        tree.insert(10)
+        tree.insert(3)
+        tree.insert(5)
+
+        assertEquals(listOf(10, 8, 5, 2, 3, 4), tree.get())
+    }
 }
